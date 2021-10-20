a2a82000546d62c9066a26a1f2a30364353acaf0
angelsflyinhell <crezoxplays@gmail.com>
2021-10-21 01:20:20 +0200

    tested a bit with ownership and borrowing
    ngl every single rust concept is so perfect

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,10 @@
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
 fn main() {
-    println!("Guess the number!");
+    let borrow_str = String::from("something");
+    let length = borrow_a_str(&borrow_str);
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("{} is {} chars long!", borrow_str, length)
+}
 
-    loop {
-        println!("Enter your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line!");
-
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue
-        };
-
-        println!("You guessed {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            }
-        }
-    }
+fn borrow_a_str(s: &String) -> usize {
+    s.len()
 }
